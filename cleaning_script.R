b6772e23e6bfcb334ce513bddad9d679fa9b2ca1
Florence Lee <florenceclee@Florence-Lees-MacBook.local>
2014-12-06 16:18:11 -0500

    ebolaData final dataset

--- cleaning_script.R
+++ cleaning_script.R
@@ -1,107 +1,154 @@
-#Check for/Install Necessary Packages
+#Check for/install necessary packages
 
-##create function
+##create function to load needed packages 
 load_install<-function(lib){
-  if(! require(lib, character.only=TRUE)) install.packages(lib, character.only=TRUE)
+  if(! require(lib, character.only=TRUE)) {
+    install.packages(lib, character.only=TRUE) }
   library(lib, character.only=TRUE)
 }
-
-##the required libraries (e.g. packages)
+##the required packages
 theLib<-c("plyr", "ggplot2", "scales", "gdata", "chron", "reshape2", "grid", "maps", "mapdata")
-
-##apply function
+##apply
 lapply(theLib, load_install)
 
-
-#Create inital data frame
+#Load data
 
 setwd("/Users/florenceclee/Desktop/qmssviz-project/") #edit to reflect your directory
 rowca <- read.csv("ebola-dat-rowca-updated.csv", stringsAsFactors=FALSE)
-who <- read.csv("ebola-dat-who.csv", stringsAsFactors=FALSE)
 
-#Use ROWCA Data set
+#Explore
 
-##general structure
 str(rowca)
+##will need to change some classes
 names(rowca)
 
-##For NAs
+##function to check for NAs
 check <- function(data) {
   NAs <- sum(is.na(data))
-  print(paste("NAs:", NAs))  # count NA's
+  print(paste("NAs:", NAs))     # count NA's
   if (NAs > 0) {
     cc <- complete.cases(data)  # logical for each case (row)
     print(paste("Complete Cases:", all(cc)))  
     data[which(!complete.cases(data)),]
   }
 }
+
 check(rowca)
+##"NAs: 0"
 
-# Subsetting data frame
+#Convert classes
 
-##General dataset
-data_use <- subset(rowca, select=c(Country, Localite, Category, Value, Date))
-head(data_use)
-
-##Liberia dataset
-liberia_sub <- subset(rowca, rowca$Country == "Liberia", select = c(Country, Localite, Category, Value, Date))
-head(liberia_sub)
-liberia_sub <- liberia_sub[order(liberia_sub$Country, liberia_sub$Localite, liberia_sub$Date),]
-#Cases = Confirmed cases + New cases + Probable cases + Suspected cases
-
-##Change Date into appropriate data type
-liberia_sub$Date <- as.Date(liberia_sub$Date, "%m/%d/%y")
-#liberia_sub$Value <- as.numeric(liberia_sub$Value)
-str(liberia_sub)
-
-##Checking unique levels of Localite
-unique(liberia_sub$Localite)
-###some Localites have trailing spaces!
-
-##Create function to trim trailing spaces
-trim <- function(df, col) {
-  #gets rid of trailing spaces
-  gsub("^\\s+|\\s+$", "", df[,col])
-}
-
-##Apply function
-liberia_sub$Localite <- trim(liberia_sub, "Localite")
-unique(liberia_sub$Localite)
-###Fixed!
-
-unique(liberia_sub$Category)
-###No problem with category
-
-##Subsetting down to only "Cases" and "Deaths"
-liberia_reduced <- subset(liberia_sub, liberia_sub$Category %in% c("Cases", "Deaths"))
-check(liberia_reduced)
-
-##Checking for Values that are not numbers (ie blanks)
-numblank <- function(df, col) {
-  blanks <- sum(df[,col]=="")
-  print(paste("Number blank:", blanks))
-  if (blanks > 0) {
-    df[which(df[, col]==""),]
-  }
-}
-
-numblank(liberia_reduced, "Value")
-#16 blanks in Values column (looks like all deaths)
-
-##Changing blanks in Value column to 0's
-for (i in 1:nrow(liberia_reduced)) {
-  if(liberia_reduced$Value[i] == "") {
-    liberia_reduced$Value[i] <- 0
-  }}
-
-##Re-checking for Value that are blanks
-numblank(liberia_reduced, "Value")
-#no blanks in Values column
-
-##Change Value into appropriate data type
-##deal with commas
+##date
+rowca$Date <- as.Date(rowca$Date, "%m/%d/%y")
+##value
 numNoCom <- function(df, col) {
   as.numeric(gsub(",", "", df[,col]))
 }
+rowca$Value <- numNoCom(rowca, "Value")
 
-liberia_reduced$Value <- numNoCom(liberia_reduced, "Value")
+rowca <- rowca[order(rowca$Country, rowca$Localite, rowca$Date),]
+##Cases = Confirmed cases + New cases + Probable cases + Suspected cases
+##okay to use Cases and Deaths only
+
+rowcaSub <- subset(rowca, rowca$Category %in% c("Cases", "Deaths"))
+
+#Clean Liberia data
+
+liberia <- subset(rowcaSub, rowcaSub$Country == "Liberia", select = c(Country, Localite, Category, Value, Date))
+head(liberia)
+
+##checking unique levels of Localite
+unique(liberia$Localite)
+###some Localites have trailing spaces
+
+##get rid of redundant County
+liberia$Localite <- gsub("(?:County)","", liberia$Localite)
+##function to trim trailing spaces
+trim <- function(x) {
+  #gets rid of trailing spaces
+  gsub("^\\s+|\\s+$", "", x)
+}
+##trim trailing spaces
+liberia$Localite <- trim(liberia$Localite)
+unique(liberia$Localite)
+
+##check unique levels of Category
+unique(liberia$Category)
+
+##subset to "Cases" and "Deaths" only
+check(liberia)
+## 18 NA values. Keep for now.
+
+liberia <- liberia[order(liberia$Country, liberia$Date),]
+
+
+#Clean Sierra Leone dataset 
+sierraLeone <- subset(rowcaSub, rowcaSub$Country == "Sierra Leone", select = c(Country, Localite, Category, Value, Date))
+
+str(sierraLeone)
+
+##checking unique levels of Localite
+unique(sierraLeone$Localite)
+##again with trailing spaces
+
+##function to capitalize first letter of each word
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=" ")
+}
+
+##fix capitalization and trailing space
+sierraLeone$Localite <- trim(sapply(sierraLeone$Localite, simpleCap))
+unique(sierraLeone$Localite)
+
+##fix 'port'
+for(i in which(sierraLeone$Localite=="Port")) {
+  sierraLeone$Localite[i] <- "Port Loko"
+}
+unique(sierraLeone$Localite)
+
+##PROBLEM
+##"Western area rural" and "Western area urban" are collapsed into one category for
+##June data. It's not a big deal, since it's only two points... but maybe we can 
+##collapse all of them?
+
+unique(sierraLeone$Category)
+###No problem with category
+
+check(sierraLeone)
+##28 NA values. Keep for now.
+
+#Clean Guinea dataset 
+guinea <- subset(rowcaSub, rowcaSub$Country == "Guinea", select = c(Country, Localite, Category, Value, Date))
+
+str(guinea)
+
+##checking unique levels of Localite
+unique(guinea$Localite)
+##again with trailing spaces
+guinea$Localite <- trim(guinea$Localite)
+
+unique(guinea$Localite)
+
+##fix weird accent translation
+for (i in which(guinea$Localite=="For\xe9cariah")) {
+  guinea$Localite[i] <- "Forecariah"
+}
+
+##noticed some have multiple Localite names
+##let's just get rid of those rows
+unique(guinea$Localite[grep("and", guinea$Localite)])
+guinea <- guinea[grep("and", guinea$Localite, invert=TRUE),]
+
+unique(guinea$Localite)
+##fixed
+
+unique(guinea$Category)
+###No problem with category
+
+check(guinea)
+##115 NA values. Keep for now.
+
+ebolaData <- as.data.frame(rbind(guinea, sierraLeone, liberia))
+
